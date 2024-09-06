44b5d5ce4803d0099786eb8c7d2f662b2a425c9f
anushaakulk <sanu210803@gmail.com>
2024-09-06 10:29:57 +0530

    Add, edit, and delete service functionality

--- src/app/component/addcategory/addcategory.component.ts
+++ src/app/component/addcategory/addcategory.component.ts
@@ -29,14 +29,14 @@ export class AddcategoryComponent implements OnInit {
   ngOnInit(): void {
 
     this.editcode = parseInt(this.act.snapshot.paramMap.get('id')!);
-    if (this.editcode != 0 && this.editcode != null) {
+    if (this.editcode != 0 && !isNaN(this.editcode)) {
       this.isedit = true
       this.title = 'Edit Category';
       this.customerform.controls['id'].disable();
       this.service.Getbycode(this.editcode).subscribe(item => {
         this.editdata = item;
         this.customerform.setValue({
-          id: this.editdata.id, cname: this.editdata.cname
+          id: this.editcode, cname: this.editdata.cname
         })
       })
     }
@@ -56,14 +56,13 @@ export class AddcategoryComponent implements OnInit {
     if (this.customerform.valid) {
 
       let _obj: category = {
-        id: this.customerform.value.id as number,
         cname: this.customerform.value.cname as string
       }
 
       if (!this.isedit) {
         this.service.Createcategory(_obj).subscribe(item => {
           this._response = item;
-          if (this._response.id) {
+          if (this._response.includes('Category')) {
             this.toastr.success('Created successfully', 'Success');
             this.router.navigateByUrl('/category');
           } else {
@@ -74,7 +73,7 @@ export class AddcategoryComponent implements OnInit {
         _obj.id=this.editcode;
         this.service.Updatecategory(_obj).subscribe(item => {
           this._response = item;
-          if (this._response.id) {
+          if (this._response) {
             this.toastr.success('Updated successfully', 'Success');
             this.router.navigateByUrl('/category');
           } else {
